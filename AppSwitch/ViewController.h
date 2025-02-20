34754606f2265a9886d496ebbcc446e64f18bd06
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-19 21:18:38 -0600

    switch

--- AppSwitch/ViewController.h
+++ AppSwitch/ViewController.h
@@ -8,6 +8,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISwitch *switchOutlet;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+- (IBAction)theSwitch:(id)sender;
 
 
 @end
--- AppSwitch/ViewController.m
+++ AppSwitch/ViewController.m
@@ -16,7 +16,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self theSwitch:nil];
 }
 
 
+- (IBAction)theSwitch:(id)sender {
+    if (self.switchOutlet.on) {
+        self.button.enabled = YES;
+        self.label.text = @"The switch is on";
+    }else {
+        self.button.enabled = NO;
+        self.label.text = @"The switch is off";
+    }
+}
 @end
